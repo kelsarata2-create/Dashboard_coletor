782dd3ff88e2ce3afc09fc04f51e092fa438b6c7
Kevin_Rosa <kelsarata2@gmail.com>
2026-09-08 21:11:41 -0300

    Update dashboard_dados.py

--- dashboard_dados.py
+++ dashboard_dados.py
@@ -1,3 +1,9 @@
-import streamlit as st
+import glob
+import os
 
-st.write('HELLO WORLD')
+
+arquivo_mais_recente = glob.glob(
+    r'C:\Users\Kevin Rosa\PycharmProjects\PythonProject\ADEUS FAB\coletor-trafego-aereo-sul\voos_sul_*.json'
+)
+print(max(arquivo_mais_recente, key=os.path.getmtime))
+
